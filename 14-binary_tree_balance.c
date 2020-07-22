2b8996f17c5d7c599ae04ce4388f64fdadc3e7c0
daniela <1469@holbertonschool.com>
2020-07-22 12:54:05 +0000

    Fixing tasks 8 and 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,29 +2,31 @@
 #define max(a, b) ((a) >= (b) ? (a) : (b))
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
+ * _binary_tree_height - function that measures the height of a binary tree
  * @tree:  is a pointer to the root node of the tree to measure the height
  * Return: tree is NULL, your function must return 0
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t _binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	return (1 + max(binary_tree_height(tree->left),
-				binary_tree_height(tree->right)));
+	return (1 + max(_binary_tree_height(tree->left),
+				_binary_tree_height(tree->right)));
 }
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
- * @tree: is a pointer to the root node of the tree to measure the balance factor
+ * binary_tree_balance - function that measures the balance
+ * factor of a binary tree
+ * @tree: is a pointer to the root node of the tree to
+ * measure the balance factor
  * Return: If tree is NULL, return 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-		if (tree==NULL)
+		if (tree == NULL)
 			return (0);
-		return (binary_tree_height(tree->left) -
-		binary_tree_height(tree->right));
+		return (_binary_tree_height(tree->left) -
+		_binary_tree_height(tree->right));
 
 }
