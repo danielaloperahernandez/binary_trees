324110b31bae802b24f24c57ddf21d5e9db21ee4
daniela <1469@holbertonschool.com>
2020-07-22 13:06:01 +0000

    Fixing task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,7 @@ size_t height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - function that finds the sibling of a node
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree: is a pointer to the root node of the tree to check
  * Return: If tree is NULL, your function must return 0
  */
@@ -31,5 +31,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (height(tree->left) != height(tree->right))
 		return (0);
 	return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->left));
+			binary_tree_is_perfect(tree->right));
 }
