77b5ea3384f21bee8a05b255c092ae83f88aeed7
daniela <1469@holbertonschool.com>
2020-07-22 13:56:37 +0000

    Adding function to 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,5 +1,22 @@
 #include "binary_trees.h"
 
+#define max(a, b) ((a) >= (b) ? (a) : (b))
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree:  is a pointer to the root node of the tree to measure the height
+ * Return: tree is NULL, your function must return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	return (1 + max(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)));
+}
+
 /**
  * func_level - function that applies the function pointer
  * to each element of the level
